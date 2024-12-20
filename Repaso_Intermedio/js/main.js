720ac2cb66d8b9735edeb7cb3f155187f52e8a10
Tania Salvatella <tsalvatellap@gmail.com>
2024-12-20 08:59:41 +0100

    start

---
+++ Repaso_Intermedio/js/main.js
@@ -0,0 +1,8 @@
+/*Piedra papel, tijera
+1. Crear html y css> select, boton, resultado y titles
+2. Al arrancar la pagina> vamos a jugar
+3. Crear numero aleatorio  de 1 y 9
+    = o < 3 --->piedra
+    = > 7 ----> papel
+    si no ---. tijera
+4. Comparar movimientos de la jugadora y el ordenador.
