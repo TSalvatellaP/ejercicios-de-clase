9c91c838b71599988a642d82410ffe0118544239
Tania Salvatella <tsalvatellap@gmail.com>
2024-12-20 10:00:15 +0100

    moveAleatory

--- Repaso_Intermedio/js/main.js
+++ Repaso_Intermedio/js/main.js
@@ -6,3 +6,43 @@
     = > 7 ----> papel
     si no ---. tijera
 4. Comparar movimientos de la jugadora y el ordenador.
+5. Traer las class al js
+*/
+
+const select = document.querySelector('.js-selector');
+const btn = document.querySelector('.js-btn');
+const start = document.querySelector('.js-start');
+const gamer = document.querySelector('.js-gamer1');
+const computer = document.querySelector('.js-computer');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+function moveAleatory () {
+    const numRandom = getRandomNumber();
+    console.log(numRandom);
+    if (numRandom <= 3) {
+        moveAleatory = stone;
+    }
+    if (numRandom => 7) {
+        moveAleatory = paper;
+    } else {
+        moveAleatory = scissor;
+    }
+
+}
+
+
+
+function handleClick(event) {
+    event.preventDefault();
+    const valueSelect = select.value;
+    //console.log(valueSelect);
+    //optionGame(valueSelect);
+    //resultGame();
+    
+}
+
+btn.addEventListener('click', handleClick);
+
